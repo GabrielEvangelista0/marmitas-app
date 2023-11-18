2fbc78c2f1f2d5c318f0ff2b1d253cbc13bfbf19
Gabriel Evangelista <86444407+GabrielEvangelista0@users.noreply.github.com>
2023-11-18 05:06:54 -0300

    update

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
-import styles from "./menu.module.css"
+import styles from "./menu.module.css";
+import { FaUser } from "react-icons/fa";
+import { IoBag } from "react-icons/io5";
 
 export default function Menu() {
     return (
@@ -7,8 +9,16 @@ export default function Menu() {
             <h1 className={styles.menu__logo}>Logo</h1>
             <nav>
                 <ul className={styles.menu__ul}>
-                    <li> <Link href={'/'}>user</Link> </li>
-                    <li> <Link href={'/'}>sacola</Link> </li>
+                    <li> <Link href={'/'} className={styles.menu__link}> <FaUser /> </Link> </li>
+                    <li>
+                        <Link href={'/'} className={`${styles.menu__link} ${styles.sacola}`}>
+                            <IoBag />
+                            <div className={styles.sacola__info}>
+                                <span>R$ 0,00</span>
+                                <span>0 itens</span>
+                            </div>
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </header>
